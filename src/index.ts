f8ada87dc1f7cf1a41b46f2f0ca8d49ff00da9c6
silvarafa06 <rafaella.silva@estudante.ifms.edu.br>
2025-08-26 11:47:32 -0300

    aula dois

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,46 @@
 import 'dotenv/config'
+import mysql from 'mysql2/promise';
 console.log(process.env.DBUSER);
 
-import express from 'express';
+import express, { Request, Response } from 'express';
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/', async (req:Request, res:Response) => {
+  if (!process.env.DBUSER){ 
+    res.status(500).send("Variavel de ambiente DBUSER não está definida");
+    return;
+  }
+  if (!process.env.DBPASSWORD&& process.env.DBPASSWORD===""){ 
+    res.status(500).send("Variavel de ambiente DBPASSWORD não está definida");
+    return;
+  }
+  if (!process.env.DBNAME){ 
+    res.status(500).send("Variavel de ambiente DBNAME não está definida");
+    return;
+  }
+  if (!process.env.DBPORT){ 
+    res.status(500).send("Variavel de ambiente DBPORT não está definida");
+    return;
+  }
+  if (!process.env.DBHOST){ 
+    res.status(500).send("Variavel de ambiente DBHOST não está definida");
+    return;
+  }
+  const connection = mysql.createConnection({
+    host: process.env.DBHOST,
+    user: process.env.DBUSER,
+    password: process.env.DBPASSWORD,
+    database: process.env.DBNAME,
+    port: Number(process.env.DBPORT),
+  }); 
+  res.send("Conectado ao banco de dados com sucesso!");
+  await connection.end(); 
   res.send(process.env.DBUSER);
-});
+}
+catch (error) {
+  res.status(500).send("Erro ao conectar ao banco de dados: " + error);
+}
+);
 app.listen(8000, () => {
     console.log('Server is running on port 8000');
 });
